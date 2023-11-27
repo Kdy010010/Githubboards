97545a28738ed6437ee0afd7b38ca8eaeb971301
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-11-27 22:39:27 +0900

    Create Min.js

---
+++ Min.js
@@ -0,0 +1,13 @@
+ (function() {
+        window.loadIframe = function(url, containerId) {
+            const iframeContainer = document.getElementById(containerId);
+            const iframe = document.createElement('iframe');
+            
+            iframe.src = url;
+            iframe.style.border = 'none';
+            iframe.width = '100%';
+            iframe.height = '100%';
+
+            iframeContainer.appendChild(iframe);
+        };
+    })();
